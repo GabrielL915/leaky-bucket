963821f5698d132b40fced0f414224a939429360
GabrielL915 <gabrielluizgomes1@gmail.com>
2025-06-09 16:23:35 -0300

    refactor entities

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -1,6 +1,18 @@
+import { Document, model, Schema } from "mongoose";
+import { DocumentBucket } from "./bucket";
 
-export interface User {
-    id: string;
+interface DocumentUser extends Document {
     username: string;
-    token: string
+    password: string;
+    token: string;
+    bucket?: DocumentBucket;
 }
+
+const userSchema = new Schema({
+    username: { require: true, type: String, unique: true },
+    password: { require: true, type: String },
+    token: { require: true, type: String }
+
+})
+
+export default model<DocumentUser>("User", userSchema);
